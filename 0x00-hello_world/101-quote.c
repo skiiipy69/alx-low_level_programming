cccd58d72fe765bed7609c6c697c5fff69a22ad4
Zouhair <darkzouhair@gmail.com>
2023-07-13 14:46:18 -0700

    fix all problem

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-
 /**
- * main - main block
- * Return: 0
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
+}
