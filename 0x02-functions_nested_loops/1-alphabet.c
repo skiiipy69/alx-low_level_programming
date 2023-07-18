0d51385546e85e25f9d29bccb89847d8c97cd264
Zouhair <darkzouhair@gmail.com>
2023-07-18 13:06:11 -0700

    task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the alphabet followed by a new line.
+*/
+void print_alphabet(void)
+{
+int alpha;
+
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+_putchar(alpha);
+_putchar('\n');
+}
+
