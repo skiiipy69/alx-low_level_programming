9f7ca01aa0cd4f6a52426a0daa66e00bd8b2c5dd
Zouhair <darkzouhair@gmail.com>
2023-07-18 12:35:34 -0700

    task 0 :0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * main - main block
- * Description: Print _putchar followed by a new line.
- * Return: 0
- */
+* main - main block
+* Description: Print _putchar followed by a new line.
+* Return: 0
+*/
 int main(void)
 {
-    char alx[] = "_putchar";
-    int i = 0;
+char alx[] = "_putchar";
+int i = 0;
 
-    while (alx[i] != '\0')
-    {
-        _putchar(alx[i]);
-        i++;
-    }
+while (alx[i] != '\0')
+{
+_putchar(alx[i]);
+i++;
+}
 
-    _putchar('\n');
-    return 0;
+_putchar('\n');
+return 0;
 }
 
