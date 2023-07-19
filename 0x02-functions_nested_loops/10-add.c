6551f493f0614350a7cc657a88cc8108ec6cbbef
Zouhair <darkzouhair@gmail.com>
2023-07-18 19:03:19 -0700

    all error fixed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum 
+*/
+
+int add(int c, int d)
+{
+	int sum = c + d;
+
+	return (sum);
+}
+
