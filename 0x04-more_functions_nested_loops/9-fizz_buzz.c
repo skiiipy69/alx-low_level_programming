e9bbb9fd2b9bfcee24161f02055cb80a5693387f
Zouhair <darkzouhair@gmail.com>
2023-07-20 16:39:31 -0700

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main block,fizz buzz from 1 to 100
+ * Description: Multiples3print Fizz. Multiples5print Buzz.
+ * Multiples3*5 should print FizzBuzz.
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 1;
+	while (a <= 100)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+
+		if (a != 100)
+			printf(" ");
+		a++;
+	}
+	printf("\n");
+
+	return (0);
+}
