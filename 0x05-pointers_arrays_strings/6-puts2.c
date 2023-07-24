bb8fe8ce131fc41a7db6ac7ee4f02afc69f04387
Zouhair <darkzouhair@gmail.com>
2023-07-24 12:24:27 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * puts2 - print one char out of 2 of a string
+ * @str: string input
+*/
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+
