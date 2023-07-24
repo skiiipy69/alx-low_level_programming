cbf9276b443652d7287d1f93c86308b70cf8a1cd
Zouhair <darkzouhair@gmail.com>
2023-07-24 12:30:51 -0700

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string input
+*/
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		;
+
+	a++;
+	for (a /= 2; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
