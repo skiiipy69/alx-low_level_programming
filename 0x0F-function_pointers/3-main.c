719f3b5e86c674a008994c08eb4bb7c1c8717b71
Zouhair <darkzouhair@gmail.com>
2023-08-16 03:37:50 -0700

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,20 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Program Entry
  *
- * Return: Always 0
- */
+ * Description: Program that takes 4 arguments
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0-on success
+*/
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int num1, num2;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -19,28 +22,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(operator)(num1, num2));
 
 	return (0);
 }
