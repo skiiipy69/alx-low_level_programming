b9bc7ff32b6fedc4da41e688f8ec339d08f80720
Zouhair <darkzouhair@gmail.com>
2023-08-15 21:02:03 -0700

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ *
+ * @name: name given
+ *
+ * @f: function of name
+ *
+ * Return: void
+ */
+ void print_name(char *name, void (*f)(char *))
+{
+		f(name);
+}
