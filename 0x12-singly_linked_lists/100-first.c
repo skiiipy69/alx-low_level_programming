9a162e653272aade4acc844d9df5471d36418b9f
Zouhair <darkzouhair@gmail.com>
2023-08-24 18:17:06 -0700

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main_p - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) main_p()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
