c181b5ee1df4ad4c261fb463fb511c3e50c9b50e
Zouhair <darkzouhair@gmail.com>
2023-08-29 09:30:19 -0700

    done

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,65 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdio.h>
 /**
- * print_listint_safe - Print a `listint_t` linked list including mem addresses
- * @head: head of linked list
- * Description: Go through the list only once.
- * Return: number of nodes in list. If fails, exit with status 98.
+ * _real - reallocates memory for an array of pointers
+ *
+ * @list: the old list to append
+ *
+ * @size: size of the new list.
+ *
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_real(const listint_t **list, size_t size,
+			const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ *
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *new_node;
-	const listint_t *temp;
-	size_t i;
+	size_t new_node, count = 0;
+	const listint_t **temp = NULL;
 
-	new_node = head;
-	if (new_node == NULL)
-		exit(98);
-
-	i = 0;
-	while (new_node != NULL)
+	while (head != NULL)
 	{
-		temp = new_node;
-		new_node = new_node->next;
-		i++;
-		printf("[%p] %d\n", (void *)temp, temp->n);
-
-		if (temp < new_node)
+		for (new_node = 0; new_node < count; new_node++)
 		{
-			printf("-> [%p] %d\n", (void *)temp, temp->n);
-			break;
+			if (head == temp[new_node])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(temp);
+				return (count);
+			}
 		}
+		count++;
+		temp = _real(temp, count, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
-	return (i);
+	free(temp);
+	return (count);
 }
