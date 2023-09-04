a671dd0c37a933912d97f8124d5bcce2185e565a
Zouhair <darkzouhair@gmail.com>
2023-09-04 07:56:29 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: filename
+ *
+ * @text_content: content writed in the file
+ *
+ * Return: 1 if success or -1 if fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int a;
+	int b;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (a == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		b = write(a, text_content, i);
+		if (b == -1)
+			return (-1);
+	}
+
+	close(a);
+	return (1);
+}
